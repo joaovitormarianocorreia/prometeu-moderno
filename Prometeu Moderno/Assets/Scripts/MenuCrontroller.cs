f8647b4137e3403279798158912556b9552e8e71
João Vitor Mariano Correia <joaovitormarianocorreia@gmail.com>
2022-02-26 18:19:51 -0300

    menu
    menu working on game scene.
    
    press the key 'q' to access menu

---
+++ Prometeu Moderno/Assets/Scripts/MenuCrontroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCrontroller : MonoBehaviour
+{
+    public string sceneName;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
