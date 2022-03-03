42e2007f1b36d32f21b67ce95f3d2125c284dc5e
João Vitor Mariano Correia <joaovitormarianocorreia@gmail.com>
2022-03-03 15:02:17 -0300

    score and cristals
    Fix score and cristals.
    
    Create HP drop and restaure with cristall collection

---
+++ Prometeu Moderno/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+    public int maxTasks = 0;
+    public int currentTasks;
+    private float healthPeriod = 0.0f;
+
+    public HealthBar healthBar;
+    public TaskBar taskBar;
+ 
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        currentTasks = maxTasks;
+        healthBar.SetHealth(currentHealth);
+        taskBar.SetTask(currentTasks);
+    }
+    
+    void Update()
+    {
+        if (healthPeriod > 5.0)
+        {
+            if (currentHealth > 0)
+            {
+                currentHealth--;
+                healthBar.SetHealth(currentHealth);
+            }
+            healthPeriod = 0;
+        }
+        healthPeriod += UnityEngine.Time.deltaTime;
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cristals")
+        {
+            other.gameObject.SetActive(false);
+            if (currentHealth < 5)
+            {
+                healthPeriod = 0;
+                currentHealth++;
+                healthBar.SetHealth(currentHealth);
+            }
+        }
+    }
+}
